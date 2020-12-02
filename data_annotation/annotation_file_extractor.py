a3f450edf56488662e95984e57bda25cd5da96a3
Sofia <syfantid@csd.auth.gr>
2020-12-02 16:18:56 +0200

    Adds support for creation of files for annotation

---
+++ data_annotation/annotation_file_extractor.py
@@ -0,0 +1,54 @@
+import errno
+import os
+
+import pandas as pd
+from pathlib import Path
+
+INPUT_PATH = 'data_collection\\output_reviews'
+
+def read_comments_from_files():
+    ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # This is your Project Root
+    absolute_input_path = os.path.join(ROOT_DIR, INPUT_PATH)
+
+    df = pd.DataFrame()
+
+    path_list = Path(absolute_input_path).rglob('*.csv')
+    for path in path_list:
+        path_str = str(path) # because path is object not string
+        file_df = pd.read_csv(path_str, index_col=0)
+        if len(df) != 0:
+            df = pd.concat([df, file_df])
+        else:
+            df = file_df
+
+    return df.reset_index(drop=True)
+
+
+def get_df_subset(df, size):
+    return df.sample(n=size, replace=False)
+
+
+def create_annotation_files(df_sample, examples_number, annotators_number):
+    output_dir_name = "output_to_annonate"
+    try:
+        os.mkdir(output_dir_name)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+    labels_names = ["participate_fishing", "learn_fisheries_species", "learn_responsible_fishing",
+                    "learn_island_life_culture", "enjoy_view_crystal_water", "visit_remote_coasts_bays",
+                    "enjoy_landscape_seascape", "observe_marine_ecosystem", "enjoy_tour_traditional_vesssel",
+                    "cook_demonstration", "swim_and_snorkel", "eat_fish", "taste_local_products", "meet_people",
+                    "hosted_authentic_friendly_environment"]
+
+    for file_counter in range(annotators_number):
+        filepath = os.path.join(os.path.join(os.getcwd(), output_dir_name), "file_" + str(file_counter) + ".xlsx")
+        df_file_sample = df_sample.sample(n=examples_number, replace=False)[["text", "title"]]
+        df_file_sample = pd.concat([df_file_sample, pd.DataFrame(columns=labels_names)], axis=1)
+        df_file_sample.to_excel(filepath, index=False, index_label=False)
+
+
+df = read_comments_from_files()
+df_sample = get_df_subset(df, 200)
+create_annotation_files(df_sample, 60, 5)

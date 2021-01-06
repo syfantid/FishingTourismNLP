fb47081c6e9505cbb6359ed43514694dc63a5fbe
Sofia <syfantid@csd.auth.gr>
2021-01-06 15:58:56 +0200

    Handles data reading for user profiling

--- text_analysis/user_predictions.py
+++ text_analysis/user_predictions.py
@@ -1,17 +1,26 @@
 import os
 
 import pandas as pd
-user_profile_analysis = True
-INPUT_FILENAME = 'processed_dataframe.csv'
 
-def setup_variables():
-    if user_profile_analysis:
-        input_filepath = 'output\\output_user_profiles'
-    else:
-        input_filepath = 'output\\output_business_profiles'
-    return input_filepath
+from text_analysis.read_comments import read_comments_from_files
+
+INPUT_PATH = 'output\\output_user_profiles'
+INPUT_FILENAME = 'processed_dataframe.csv'
+DEMOGRAPHICS_PATH = 'data_collection\\output_demographics'
+
+
+def gender_prediction(df):
+    df.dropna(subset=['gender'], inplace=True)
+    print()
+
 
 if __name__ == '__main__':
-    input_filepath = setup_variables()
-    df = pd.read_csv(os.path.join(input_filepath, INPUT_FILENAME))
+    df_comments = pd.read_csv(os.path.join(INPUT_PATH, INPUT_FILENAME))
+    df_demographics = read_comments_from_files(DEMOGRAPHICS_PATH, user_profiles=True)
+    # Join dataframes
+    df = df_comments.merge(df_demographics, on='username', how='inner')
+
+    # Gender Prediction Process
+    gender_prediction(df)
+
     print()

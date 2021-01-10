d5e85973813790a29a78e227c3b8a20359ca60a6
demeter <dkaranatsiou@gmail.com>
2021-01-10 18:56:44 +0200

    files added

--- text_analysis/user_predictions.py
+++ text_analysis/user_predictions.py
@@ -1,6 +1,8 @@
 import os
 from pprint import pprint
 from time import time
+import random
+import re
 
 import pandas as pd
 from matplotlib.pyplot import bar
@@ -18,18 +20,25 @@ from text_analysis.read_comments import read_comments_from_files
 
 from spellchecker import SpellChecker
 from nltk import pos_tag
+from nltk.corpus import names
+from nltk import sent_tokenize
+from nltk import NaiveBayesClassifier
 import textstat
 from text_analysis.sentiment_analysis import get_review_sentiment
+
+from imblearn.over_sampling import SMOTE
+
 
 INPUT_PATH = 'output\\output_user_profiles'
 INPUT_FILENAME = 'processed_dataframe.csv'
 DEMOGRAPHICS_PATH = 'data_collection\\output_demographics'
+MODELS_PATH = '' #TODO add Sofia
 
 # For mac
 # INPUT_PATH = 'text_analysis/output/output_user_profiles'
 # INPUT_FILENAME = 'processed_dataframe.csv'
 # DEMOGRAPHICS_PATH = 'FishingTourismNLP/data_collection/output_demographics'
-
+# MODELS_PATH = 'models'
 
 
 def grid_search(pipeline, parameters, X_train, y_train, filename):
@@ -76,6 +85,7 @@ def fix_labels(df):
     return df
 
 
+
 def nb(X_train, X_test, y_train, y_test):
     pipeline_nb = Pipeline([
         ('vect', CountVectorizer()),
@@ -85,6 +95,7 @@ def nb(X_train, X_test, y_train, y_test):
     clf_nb = pipeline_nb.fit(X_train, y_train)
     y_predicted = clf_nb.predict(X_test)
     evaluate(y_test, y_predicted, clf_nb, X_test, "Multinomial Bernoulli")
+
 
 
 def lr(X_train, X_test, y_train, y_test):
@@ -165,13 +176,15 @@ def age_features(df):
     df['no_char'] = df['review_details'].str.len()  #number of characters
     df['no_words'] = df['review_details'].str.split().str.len() #number of words
 
-    def average_words(sentences):
+    def average_words(text):
         counts = []
+        sentences = sent_tokenize(text)
         for sentence in sentences:
             counts.append(sentence.split())
-        return float(sum(counts) / len(counts))
+        words = sum([len(element) for element in counts])
+        return float(words)/len(counts)
 
-    #df['sentence_words'] = df['review_details'].str.strip().apply(lambda x: average_words(x)) #avg number of words per sentence
+    df['sentence_words'] = df['review_details'].apply(lambda x: average_words(x)) #avg number of words per sentence
     df['exclamatories'] = df['review_details'].str.count('!') #number of exclamatories
     #df['no_misspelled'] = df['review_details'].str.split().apply(lambda x: len(list(SpellChecker.unknown(x)))) #number of misspelled words
 
@@ -202,8 +215,75 @@ def age_features(df):
 
     return df
 
+def clean_username(name):
+    cleaned = re.sub(r'[?|!|\'|"|#]', r'', name)
+    cleaned = re.sub(r'[.|,|)|(|\|/|@]', r' ', cleaned)
+    cleaned = re.sub(r'.csv', r'', cleaned)
+    cleaned = re.sub(r'\d+', r'', cleaned)
+
+    return cleaned
+
+def name_extract_features(name):
+    name = name.lower()
+    return {
+        'last_char': name[-1],
+        'last_two': name[-2:],
+        'last_three': name[-3:],
+        'first': name[0],
+        'first2': name[:1]
+    }
+
+def gender_name_training(name):
+    # preparing a list of examples and corresponding class labels.
+    labeled_names = ([(name, 'male') for name in names.words('male.txt')] +
+                 [(name, 'female') for name in names.words('female.txt')])
+
+    random.shuffle(labeled_names)
+
+    # we use the feature extractor to process the names data.
+    featuresets = [(name_extract_features(n), gender)
+               for (n, gender) in labeled_names]
+
+    # Divide the resulting list of feature
+    # sets into a training set and a test set.
+    train_set, test_set = featuresets[500:], featuresets[:500]
+
+    # The training set is used to train a new "naive Bayes" classifier.
+    gender_clf = NaiveBayesClassifier.train(train_set)
+
+    # save the model to disk
+    filename = 'gender_name_model.sav'
+    pickle.dump(gender_clf, open(filename, 'wb'))
+
+    return gender_clf
+
+
 def gender_features(df):
 
+    # gender estimation from username
+    df['clean_username'] = df['username'].apply(lambda x: clean_username(x)) #clean username
+    gender_clf = pickle.load(open(os.pathconf(MODELS_PATH, 'gender_name_model.sav').sav, 'rb')) # load model
+    df['gender_estimation'] = df['clean_username'].apply(lambda x: gender_clf.classify(name_extract_features(x)))
+
+    # Sentiment
+    df['text_sentiment'] = df['review_details'].apply(lambda x: get_review_sentiment(x))
+
+    # syntax
+    df['pos_tag'] = df['text_p'].apply(lambda x: pos_tag(x.split(" ")))
+    df['tags'] = df['pos_tag'].apply(lambda x: [pos for word, pos in (x)])
+    # # summing to larger POS groups
+    df['Adj'] = df['tags'].apply(lambda x: x.count('JJ') + x.count('JJR') + x.count('JJS'))
+    df['Verb'] = df['tags'].apply(
+        lambda x: x.count('VB') + x.count('VBD') + x.count('VBG') + x.count('VBN') + x.count('VBP') + x.count('VBZ'))
+    df['Noun'] = df['tags'].apply(lambda x: x.count('NN') + x.count('NNS') + x.count('NNP') + x.count('NNPS'))
+    df['Adv'] = df['tags'].apply(lambda x: x.count('RB') + x.count('RBR') + x.count('RBS'))
+
+    #politeness
+
+    # found this https://github.com/ryandavila/new-politeness but doesn't run
+
+
+    return df
 
 
 
@@ -215,18 +295,24 @@ def age_prediction(df, classifier='lr'):
     X = df['text_p'] #age features extraction
     y = df['age']
 
+    # Results without oversampling and only cv - F1 macro
+    # NB: 0.64, LR: 0.66, RF: 0.54
+
+    # using synthetic oversampling technique
+    smote = SMOTE('minority')
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=33)
-    # gender_prediction_nb(X_train, X_test, y_train, y_test)
-    # gender_prediction_lr(X_train, X_test, y_train, y_test)
+
+    X_sm, y_sm = smote.fit(X_train, y_train)
+
     if classifier == 'nb':
-        nb(X_train, X_test, y_train, y_test)
+        nb(X_sm, X_test, y_sm, y_test)
     elif classifier == 'lr':
-        lr(X_train, X_test, y_train, y_test)
+        lr(X_sm, X_test, y_sm, y_test)
     elif classifier == 'rf':
-        rf(X_train, X_test, y_train, y_test)
+        rf(X_sm, X_test, y_sm, y_test)
     else:
-        sgd(X_train, X_test, y_train, y_test)
+        sgd(X_sm, X_test, y_sm, y_test)
     # param_grid = [{'logisticregression__C': [1, 10, 100, 1000]}
     # gs = GridSearchCV(pipe, param_grid)
     # gs.fit(X, y)
